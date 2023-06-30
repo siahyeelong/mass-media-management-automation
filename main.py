09cc28c20ba3551fe202831c4c4f8648ddbf94b3
siahyeelong <100999280+siahyeelong@users.noreply.github.com>
2023-06-30 08:09:03 +0800

    typo

--- main.py
+++ main.py
@@ -71,7 +71,7 @@ def tally(ext):
         count_mp4 += 1
     elif ext.lower() == ".heic":
         count_heic += 1
-    elif ex.lower() == ".json":
+    elif ext.lower() == ".json":
         0
     else:
         count_others += 1
