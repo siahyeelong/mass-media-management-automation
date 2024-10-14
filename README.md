f43ba87c12643495ab56881c887b3eb0656c9419
Yee Long Siah <yeelongsiah@Yees-MacBook-Air.local>
2024-10-14 16:12:17 +0800

    slight reorg

--- README.md
+++ README.md
@@ -1,5 +1,17 @@
 # Mass media management automation
 
-Downloading my photos from Google Photos, I realised that there was no coherent naming convention.
+The purpose of this project was to name my photos and videos downloaded from Google Photos using the Takeout function. The main.py script simply sorts all json files into a folder and renames all photos and videos according to the timestamp saved in its corresponding metadata.
 
-To overcome this, I wanted to build something that would automatically rename my photos to match the date that it was created. Luckily, the downloaded data from Google Photos comes with a JSON metadata file for each photo, which contains useful information that I might use in the future -- such as to further sort my photos by location.
+# Usage
+## Downloading photos from takeout
+From takeout.google.com, select Google Photos and request for a takeout of all your google photos. It will take a while depending on how many photos and videos you store, but eventually the link for the download should be sent to your email. Download your media from the link provided
+
+## Locating the folder
+Locate your photos in the Downloads folder. Copy the path of the folder.
+
+## Run the main script
+In the terminal, run 
+```bash 
+python main.py
+``` 
+As prompted, paste the path of the folder you just copied. At this point, the script will rename the media and sort all json files into the `metadata` folder
--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 import os
 import json
 import pathlib
-from datetime import datetime
+from datetime import datetime, timezone
 import shutil
 
 
-# convert unix time format to human readable format
+# convert unix time format to ISO format
 def unixTodate(unix):
-    return datetime.utcfromtimestamp(unix).strftime("%Y%m%d_%H%M%S")
+    return datetime.fromtimestamp(unix, timezone.utc).strftime("%Y%m%d_%H%M%S")
 
 
 # function that does the renaming
@@ -77,91 +77,90 @@ def tally(ext):
         count_others += 1
 
 
-# ask for folder path
-root_dir = input("Enter file path: ")
-meta_folder = root_dir + "/metadata"
+if __name__ == "__main__":
+    # ask for folder path
+    root_dir = input("Enter the path of the folder where the photos are stored: ")
+    root_dir = root_dir.strip("'")
+    meta_folder = root_dir + "/metadata"
 
-# check if metadata has been moved alr
-if os.path.exists(meta_folder):
-    print("exists")
+    # check if metadata has been moved alr
+    if os.path.exists(meta_folder):
+        print("exists")
 
-else:
-    os.makedirs(meta_folder)
-
-    for path in pathlib.Path(root_dir).iterdir():
-        fileName, fileExtension = os.path.splitext(path)
-        if fileExtension == ".json":
-            count_jsonfiles += 1
-            shutil.move(path, meta_folder)
-
-# iterate through every json file, rename accordingly
-
-for meta in pathlib.Path(meta_folder).iterdir():
-    f = open(meta)
-    data = json.load(f)
-    try:
-        timestamp = unixTodate(int(data["photoTakenTime"]["timestamp"]))
-    except:
-        continue
-    if (
-        meta.name[:-5][-1:] == ")"
-    ):  # for names like lp_image.heic(32).json and IMG_5881.HEIC(1).json
-        n = meta.name[:-5][:-1].split("(")[1]
-        title = (
-            meta.name.split(".")[0]
-            + "("
-            + n
-            + ")"
-            + "."
-            + meta.name.split(".")[1].split("(")[0]
-        )
     else:
-        title = data["title"]
+        os.makedirs(meta_folder)
 
-    if (
-        title[:8] == "lp_image" or title[:4] == "IMG_"
-    ):  # these two file names tend to have a .MP4 file along with the original image file because theyre live photos
-        ext = "." + title.split(".")[1]
-        renamingFunction(root_dir, timestamp, title, ext)
-        title = title.split(".")[0] + ".MP4"
-        ext = ".MP4"
-    else:
-        ext = "." + title.split(".")[1]
+        for path in pathlib.Path(root_dir).iterdir():
+            fileName, fileExtension = os.path.splitext(path)
+            if fileExtension == ".json":
+                count_jsonfiles += 1
+                shutil.move(path, meta_folder)
 
-    if renamingFunction(root_dir, timestamp, title, ext) == 0:
-        # try the same title but with MP4 extension again
+    # iterate through every json file, rename accordingly
+    for meta in pathlib.Path(meta_folder).iterdir():
+        f = open(meta)
+        data = json.load(f)
         try:
+            timestamp = unixTodate(int(data["photoTakenTime"]["timestamp"]))
+        except:
+            continue
+        if (
+            meta.name[:-5][-1:] == ")"
+        ):  # for names like lp_image.heic(32).json and IMG_5881.HEIC(1).json
+            n = meta.name[:-5][:-1].split("(")[1]
+            title = (
+                meta.name.split(".")[0]
+                + "("
+                + n
+                + ")"
+                + "."
+                + meta.name.split(".")[1].split("(")[0]
+            )
+        else:
+            title = data["title"]
+
+        if (
+            title[:8] == "lp_image" or title[:4] == "IMG_"
+        ):  # these two file names tend to have a .MP4 file along with the original image file because theyre live photos
+            ext = "." + title.split(".")[1]
+            renamingFunction(root_dir, timestamp, title, ext)
             title = title.split(".")[0] + ".MP4"
             ext = ".MP4"
-            renamingFunction(root_dir, timestamp, title, ext)
+        else:
+            ext = "." + title.split(".")[1]
 
-        except:
-            print("not found", title)
+        if renamingFunction(root_dir, timestamp, title, ext) == 0:
+            # try the same title but with MP4 extension again
+            try:
+                title = title.split(".")[0] + ".MP4"
+                ext = ".MP4"
+                renamingFunction(root_dir, timestamp, title, ext)
 
-    # rename JSONs
-    ext = ".json"
-    renamingFunction(meta_folder, timestamp, meta.name, ext)
+            except:
+                print("not found", title)
 
-    f.close()
+        # rename JSONs
+        ext = ".json"
+        renamingFunction(meta_folder, timestamp, meta.name, ext)
 
-print("JSON files:", count_jsonfiles)
-print("MOV files:", count_mov)
-print("MP4 files:", count_mp4)
-print("GIF files:", count_gif)
-print("JPEG files:", count_jpeg)
-print("PNG files:", count_png)
-print("HEIC files:", count_heic)
-print("Other files:", count_others)
-print(
-    "\nTotal media:",
-    count_others
-    + count_gif
-    + count_png
-    + count_jpeg
-    + count_gif
-    + count_mp4
-    + count_mov
-    + count_heic,
-)
+        f.close()
 
-##
+    print("JSON files:", count_jsonfiles)
+    print("MOV files:", count_mov)
+    print("MP4 files:", count_mp4)
+    print("GIF files:", count_gif)
+    print("JPEG files:", count_jpeg)
+    print("PNG files:", count_png)
+    print("HEIC files:", count_heic)
+    print("Other files:", count_others)
+    print(
+        "\nTotal media:",
+        count_others
+        + count_gif
+        + count_png
+        + count_jpeg
+        + count_gif
+        + count_mp4
+        + count_mov
+        + count_heic,
+    )
